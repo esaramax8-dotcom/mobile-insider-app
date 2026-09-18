b5913e7e8b42a86837b5a7510411c71bc14a2a25
Esara Akashmitha <esaramax8@gmail.com>
2026-09-18 22:19:50 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/mobileinsider/app/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mobileinsider.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.mobileinsider.app.navigation.AppNavigation
+import com.mobileinsider.app.ui.theme.MobileInsiderTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            MobileInsiderTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    AppNavigation(
+                        navController =
+                            rememberNavController()
+                    )
+                }
+            }
+        }
+    }
+}
